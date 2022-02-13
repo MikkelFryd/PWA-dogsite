9c43c8ceb75ce180941d37311b2aa9dba3bf8e72
mikk7021 <mikk7021@gmail.com>
2022-02-13 13:48:36 +0100

    pushed

--- index.js
+++ index.js
@@ -16,3 +16,29 @@ if ("serviceWorker" in navigator) {
     alert("pwa not supported")
 }
 
+const image = document.getElementById('dogimg')
+const dogurl = 'https://dog.ceo/api/breeds/image/random'
+
+getDogs(dogurl)
+
+function getDogs(dogurl) {
+
+
+    fetch(dogurl)
+    .then(res => res.json())
+    .then(json => save(json))
+
+    .catch(err => {
+        window.location.href = "https://mikk7021.github.io/PWA-dogsite/info.html"
+        const offlinehtml = `
+            <h2>Du er desværre offline<h2/>
+            <img src="./assets/images/fetchimage.png"></img>
+        `
+        const main = document.querySelector('main')
+        main.innerHTML = offlinehtml
+    })
+
+    function save(result) {
+        image.src = result.message
+    }
+}
--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,7 @@
 let CACHE_NAME = 'my-site-cache';
 
 let urlsToCache = [
+    './',
     './main.css',
     './assets/images/dogicon-192x192.png',
     './assets/images/dogicon-512x512.png',
@@ -13,24 +14,14 @@ let urlsToCache = [
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
-
         .then(function(cache) {
             console.log('Opened cache')
-
             return cache.addAll(urlsToCache)
         })
         )
     })
     
     self.addEventListener('fetch', function(e) {
-        e.respondWith(
-            caches.open('my-site-cache').then(function (cache) {
-            return fetch(e.request).then(function (response) {
-                cache.add(response)
-                console.log(response)
-                return response
-            })
-        })
-    )
-})
+        console.log('intercept req:' +e.request.url);
 
+})
